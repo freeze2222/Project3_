6f06fbbbaa6d8222478c8fa210a37885c9f86e1c
freeze2222 <opop1094@gmail.com>
2021-10-10 11:54:44 +0300

    b

---
+++ src/com/samsung/AcMod.java
@@ -0,0 +1,10 @@
+package com.samsung;
+
+public class AcMod {
+    /*
+  |>1.Public    all packages
+  |>2.Protected only file and inherited classes
+  | 3.Private   only file
+  --4.default   only package
+     */
+}
