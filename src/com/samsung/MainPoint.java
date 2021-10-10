4530d3c763793f70db568dbc8f664cee69cb6dd2
freeze2222 <opop1094@gmail.com>
2021-10-10 11:43:27 +0300

    a

--- src/com/samsung/MainPoint.java
+++ src/com/samsung/MainPoint.java
@@ -3,7 +3,7 @@ package com.samsung;
 public class MainPoint {
     public static void main(String[] args) {
 
-        Point point = new Point(4, 4);
+        Point point = new Point(5, 5);
         System.out.println(point.A() + " " + point.L());
     }
 }
